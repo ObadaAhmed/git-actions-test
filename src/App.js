d05fea2d9feaa0dcbb4a9cc7d96a3e3fa1252179
Obada-Ahmed <144023346+Obada-Ahmed@users.noreply.github.com>
2024-03-11 12:45:12 +0400

    modify message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing Alef Matrix pipeLine using github actions
         </a>
       </header>
     </div>
